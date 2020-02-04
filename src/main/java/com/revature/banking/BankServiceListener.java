ed8ff6078e9181d2245c077aa672b374c47023d5
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2020-02-04 06:23:53 -0500

    Minor modifications to previous code

--- src/main/java/com/revature/banking/BankServiceListener.java
+++ src/main/java/com/revature/banking/BankServiceListener.java
@@ -1,5 +1,19 @@
 package com.revature.banking;
-//TODO  Probably a Thread based code allowing to continuously be able to read input from the console. A listener, that never stops, and read all inputs from the bank console. The listener starts on the main page and keeps in memory the page the user is on. After having received the user input, a request is sent to the logic layer to get the next data to display. --> 2 parameters: page_id + option chosen by the user.
-public class BankServiceListener {
+//TODO  Probably a Thread based code allowing to continuously be able to read input from the console. A listener, that never stops, and read all inputs from the bank console. The listener starts on the main page and keeps in memory the page the user is on. After having received the user input, a request is sent to the logic layer to get the next data to display. --> 2 parameters: page_name + option chosen by the user.
+public class BankServiceListener implements Runnable {
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("The Thread is running");
+		
+	}
+	
+	public static void main(String[] args) {
+		BankServiceListener myBankServiceListener = new BankServiceListener();
+		Thread myThread = new Thread(myBankServiceListener);
+		
+	}
+	
 
 }
--- src/main/java/com/revature/banking/LogicLayer.java
+++ src/main/java/com/revature/banking/LogicLayer.java
@@ -6,10 +6,14 @@ package com.revature.banking;
 public class LogicLayer {
 	
 	
-	public String answerUserRequest(String userdata)
+	public String answerUserRequest(String page_name, String userdata)
 	{
-		//The LogicLayer receives a user input, e.g. "Login". and gets the data to display, in this case the login data to display on the interface.
+		//The LogicLayer receives a user input, e.g. "2", for "Create an account", and  the page of origin, e.g. "home" and gets the data to display, in this case the login data to display on the interface.
+		// TODO: use a of servlet? for the pages logic?
+		// TODO: Persistence of the information mapping a page and an option selected to a data to return and display.
 		
+		switch(page-answer):
+			
 		return null;
 	}
 
